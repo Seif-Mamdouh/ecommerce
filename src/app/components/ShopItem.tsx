b2f3d9aa468fa50fcc15ab76f2e1b5ca3b30413f
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-03-30 00:27:28 -0400

    add to card done

--- src/app/components/ShopItem.tsx
+++ src/app/components/ShopItem.tsx
@@ -4,24 +4,28 @@ import React from 'react'
 import '../styles/styles.css';
 
 interface ShopItemProps {
-    title: string;
-    price: number;
-    rating: number;
-    brand: string;
-    thumbnail: string;
+  title: string;
+  price: number;
+  rating: number;
+  brand: string;
+  thumbnail: string;
+  addToCart: () => void;
 }
 
-
-
 const ShopItem = (props: ShopItemProps) => {
+
+  const handleAddToCart = () => {
+    props.addToCart();
+  }
+
   return (
     <div className="shop-card">
-      <img src={props.thumbnail} alt={props.title} />
-      <div>{props.title}</div>
-      <div>${props.price}</div>
-      <div>rating: {props.rating}</div>
-      <div>brand: {props.brand}</div>
-      title
+      <img src={props.thumbnail} alt={props.title} /> 
+      <h6>{props.title}</h6>
+      <h6>${props.price}</h6>
+      <h6>rating: {props.rating}</h6>
+      <h6>brand: {props.brand}</h6> 
+      <button onClick={handleAddToCart}> Add To Cart </button>
     </div>
   );
 }
