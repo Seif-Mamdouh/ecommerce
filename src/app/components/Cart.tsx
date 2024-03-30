2d40899351664689637e736fca8cc0def833153a
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-03-30 12:40:17 -0400

    refactoring the main comoponent logic

--- src/app/components/Cart.tsx
+++ src/app/components/Cart.tsx
@@ -1,13 +1,27 @@
 "use client";
 import React from 'react'
 
+interface CartProps {
+  cart: any;
+  totalCartValue: number;
+}
 
-const Cart = () => {
+const Cart = (props: CartProps) => {
   return (
-    <div className = "cart">
-      Cart
+    <div className="cart">
+      <h3>Cart</h3>
+      <ul>
+        {props.cart.map((item: any) => (
+          <li key={item.id}>
+            <h6>{item.title}</h6>
+            <h6>Quantity: {item.quantity}</h6>
+            <h6>Price: ${item.totalPrice}</h6>
+          </li>
+        ))}
+      </ul>
+      <h6>Total: ${props.totalCartValue}</h6>
     </div>
-  )
+  );
 }
 
 export default Cart
--- src/app/components/Shop.tsx
+++ src/app/components/Shop.tsx
@@ -5,60 +5,27 @@ import ShopItem from "./ShopItem";
 import "../styles/styles.css";
 import Filter from "./Filter";
 import Cart from "./Cart";
+import useShoppingCart from "./useShoppingCart";
 
 const Shop = () => {
-  const [state, setState] = React.useState([]);
-  const [error, setError] = React.useState(null);
-  const [filter, setFilter] = React.useState([]);
-  const [cart, setCart] = React.useState([]);
 
-React.useEffect(() => {
-  const limit = 9;
+    const { state, handleFilter, addToCart } = useShoppingCart();
+    const { data, error, filter, cart, totalCartValue } = state;
 
-  fetch(`https://dummyjson.com/products?limit=${limit}`)
-    .then((response) => response.json())
-    .then(
-      (data) => {
-        setState(data.products);
-      },
-      (error) => setError(error)
-    );
-}, []);
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
-  if (state.length === 0) {
-    return <div>Loading...</div>;
-  }
-
-  const handleFilter = (category) => {
-    if (filter.includes(category)) {
-      setFilter(filter.filter((item) => item !== category));
-    } else {
-      setFilter([...filter, category]);
+    if (error) {
+      return <div>Error: {error.message}</div>;
     }
-  };
 
-  const addToCart = (itemToAdd) => {
-    const existingCartItemIndex = cart.findIndex(
-      (item) => item.id === itemToAdd.id
-    );
-    if (existingCartItemIndex !== -1) {
-      const updatedCart = [...cart];
-      updatedCart[existingCartItemIndex].quantity += 1;
-      setCart(updatedCart);
-    } else {
-      setCart([...cart, { ...itemToAdd, quantity: 1 }]);
+    if (data.length === 0) {
+      return <div>Loading...</div>;
     }
-  };
+
 
   return (
     <div className="container">
-      <div className="filter-container">
-        {state &&
-          Array.from(new Set(state.map((item) => item.category))).map(
+      <div>
+        {data &&
+          Array.from(new Set(data.map((item) => item.category))).map(
             (category) => (
               <div key={category}>
                 <Filter
@@ -71,11 +38,9 @@ React.useEffect(() => {
           )}
       </div>
       <div className="shop-items-container">
-        {state &&
-          state
-            .filter(
-              (item) => filter.length === 0 || filter.includes(item.category)
-            )
+        {data &&
+          data
+            .filter((item) => filter.length === 0 || filter.includes(item.category))
             .map((item) => (
               <div key={item.id}>
                 <ShopItem
@@ -90,14 +55,7 @@ React.useEffect(() => {
             ))}
       </div>
       <div className="cart-container">
-        <h2>Shopping Cart</h2>
-        <ul>
-          {cart.map((item) => (
-            <li key={item.id}>
-              {item.title} - ${item.price} - Quantity: {item.quantity}
-            </li>
-          ))}
-        </ul>
+        <Cart cart={cart} totalCartValue={totalCartValue} />
       </div>
     </div>
   );
